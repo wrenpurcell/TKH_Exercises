2af018a058da597b0d157ea10ee9934decf8eeae
Jonathanh7 <Jonathanh949@gmail.com>
2021-06-03 00:29:53 -0400

    created exercises

---
+++ main.js
@@ -0,0 +1,45 @@
+// 1.
+//  Using the filter method, filter out the sedans.
+// output =>
+//  { brand: 'Toyota', model: 'camry', type: 'sedan' },
+//   { brand: 'Hyundai', model: 'Sonata', type: 'sedan' }
+const carBrands = [
+  { brand: "Ford", model: "mustang", type: "convertible" },
+  { brand: "Toyota", model: "camry", type: "sedan" },
+  { brand: "Ram", model: "1500", type: "pickup" },
+  { brand: "Hyundai", model: "Sonata", type: "sedan" },
+  { brand: "Jeep", model: "wrangler", type: "suv" },
+  { brand: "Nissan", model: "frontier", type: "pickup" },
+];
+
+/* 2.
+ * reverseString takes a string
+ * and should return the reverse of the string, you cannot use .reverse method
+ * e.g., reverseString('cat') => 'tac'
+ */
+
+
+
+// 3.
+// Using Reduce,
+// Given an array of all your wishlist items, figure out
+// how much it would cost to just buy everything at once
+// In other words, the total of all the prices in the array
+// of objects
+// The output should eqaute to 227005
+let wishlist = [
+  { title: "tesla", price: 90000 },
+  { title: "tesla", price: 45000 },
+  { title: "tesla", price: 5 },
+  { title: "tesla", price: 2000 },
+  { title: "tesla", price: 90000 },
+];
+function shop(arr) {}
+console.log(shop(wishlist));
+
+
+// 4.
+//Explain recursion in your own words,
+// give an example of a recursive algorithm, and explain how it works
+//Make sure to detail the steps that make up a recursive algorithm
+// A detailed explanation.
